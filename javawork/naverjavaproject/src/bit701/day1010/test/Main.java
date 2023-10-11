2d57bd7203b45d3a898bfbbf7bb639945a369e32
dong <kd0948@gmail.com>
2023-10-11 17:21:28 +0900

    1011

---
+++ javawork/naverjavaproject/src/bit701/day1010/test/Main.java
@@ -0,0 +1,16 @@
+package bit701.day1010.test;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextInt();
+		long b = sc.nextInt();
+		long c = sc.nextInt();
+		System.out.println(a+b+c);
+	}
+
+}
