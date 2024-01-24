8acfb11f2c3f2dd8d50cf94ee38745f53974e7c8
김동현 <kd0948@naver.com>
2024-01-24 18:55:44 +0900

    0124-리액트-로그인-시큐리티

--- bootwork/MybatisBootJwt/src/main/java/data/service/MemberService.java
+++ bootwork/MybatisBootJwt/src/main/java/data/service/MemberService.java
@@ -29,4 +29,24 @@ public class MemberService {
 		 memberMapper.insertMember(dto);
 	 }
 	 
+	 public int getIdCheck(String myid)
+	 {
+		 return memberMapper.getIdCheck(myid);
+	 }
+	 
+	 public void deleteMember(int num) {
+		 memberMapper.deleteMember(num);
+	 }
+	 
+	 public MemberDto getMember(int num)
+	 {
+		 return memberMapper.getMember(num);
+	 }
+	 
+	 public MemberDto getLogin(String myid)
+	 {
+		 return memberMapper.getLogin(myid);
+	 }
+
+	 
 }
